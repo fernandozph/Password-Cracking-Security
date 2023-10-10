704bc131ddde518878d841babbc9c8ce7e075975
John Gador <jgador@acl203.cs.vassar.edu>
2023-10-10 17:12:26 -0400

    int instead of char for genInt()

--- intellij/src/GeneratePassword.java
+++ intellij/src/GeneratePassword.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3be1c3c24c0a585b0511de5fe1d7f3c445ad8d7d5a2ece6601ff57c92369531
-size 1652
+oid sha256:e42b24b4cf6e4126fcf6087dcf4c4aad72358517063f87e7ec8e93fe5fbd5fcb
+size 1651
