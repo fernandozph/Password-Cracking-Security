fa4ed532ecae98259e52419145087f07c367d2aa
John Gador <jgador@acl203.cs.vassar.edu>
2023-10-12 21:13:12 -0400

    ugh

--- intellij/src/Characters.java
+++ intellij/src/Characters.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0654b024d42b3f885a0d5d93e1386748fe7405c4d337c8b4728a43b8a7f784f
-size 1131
+oid sha256:a52a34ed0a061f51292b976dbb6948ae5384aa5697c05956b4c63b086224df28
+size 934
