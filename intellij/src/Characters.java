a124f0c8bf96d5b3c0c78350c0bd9304986dbfff
John Gador <jgador@acl203.cs.vassar.edu>
2023-10-12 21:37:44 -0400

    Really basic Characters.java

--- intellij/src/Characters.java
+++ intellij/src/Characters.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2849037edf464e5d227f2604fb7cdf8f585230444b97fe574347290153d145d
-size 997
+oid sha256:52a2f72c885b28384c2f5c54fbc7c01e325dce88e7728f58a91651a5e13c427e
+size 1445
