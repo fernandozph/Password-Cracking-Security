278b0d85922162686474b4826aa5bed46286441f
John Gador <jgador@acl203.cs.vassar.edu>
2023-10-17 14:26:27 -0400

    uncomment

--- intellij/src/main.java
+++ intellij/src/main.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aadb94f2f44a8b0aa710896db5a958a04ef8b783641ec6990d91b0d845778b69
-size 538
+oid sha256:5f84a7f030e4bbc503236d326a415deead6a80b673d08c4071a83862b08148c7
+size 570
