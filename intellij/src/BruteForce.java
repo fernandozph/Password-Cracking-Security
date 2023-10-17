5a323d8f3c210cfeb1933183f4a09ace108e7410
Fernando <nandoz1ph@gmail.com>
2023-10-17 17:21:06 -0400

    Trying to use arrays again, will switch back to ArrayList from here :(

--- intellij/src/BruteForce.java
+++ intellij/src/BruteForce.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0177e4456f1bdcd0b6330a872a7e3422bec15cd042ca914034e59bb4e2142f95
-size 2726
+oid sha256:ca03a0c47f1c450dfcd19bb7698f72862ed642b4b9a30e2fdcad587cb6b7a442
+size 3168
