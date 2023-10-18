6f78e4cff07f3ce0c4fa122f46e90b0c5d4b23d6
Fernando <nandoz1ph@gmail.com>
2023-10-17 22:50:29 -0400

    Finished iteration 1 of brute force, can now crack passwords (is very slow though). added test in main method

--- intellij/src/BruteForce.java
+++ intellij/src/BruteForce.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca03a0c47f1c450dfcd19bb7698f72862ed642b4b9a30e2fdcad587cb6b7a442
-size 3168
+oid sha256:28dfd493f87c2e02310b1815bdd8e06b3dc6d706aef15926a9dfc71dab25d523
+size 4064
--- intellij/src/main.java
+++ intellij/src/main.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f84a7f030e4bbc503236d326a415deead6a80b673d08c4071a83862b08148c7
-size 570
+oid sha256:41a4c8c780f8cdd6172281976af2e257b5acce43eb6b11f66cf1dc9a818dc872
+size 773
