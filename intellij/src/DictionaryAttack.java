5945b7b3c1ef4484cd8a6c554640000911e6468b
Fernando <nandoz1ph@gmail.com>
2023-10-19 21:10:40 -0400

    Generalized file path into input from constructor

--- intellij/src/DictionaryAttack.java
+++ intellij/src/DictionaryAttack.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5316d8a4ec723a0cbfae3b79972fff79ca75f7d9f63aae461f0c313f132f7a09
-size 2400
+oid sha256:7b9420a464e348087e61ddc3f4a8e94af008c8276e8fae7f08fa317875671cfa
+size 2433
