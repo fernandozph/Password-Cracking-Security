b464dcc375c97d538425d1e6d01a2155d10ad82f
John Gador <jgador@acl203.cs.vassar.edu>
2023-10-21 21:01:21 -0400

    testing for arguments

--- intellij/src/Controller.java
+++ intellij/src/Controller.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a15aded1fc11ba687a4de5061616392be44db6f7a280871bdf3029a00dadabd
-size 3818
+oid sha256:3af8b11e34f242f75797caac92f38a76b06985de2d907b523382961aede8798e
+size 3942
