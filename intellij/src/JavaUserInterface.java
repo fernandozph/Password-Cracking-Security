bb10161c4f3f235a4245936b3ee3c604a84987a6
Fernando <nandoz1ph@gmail.com>
2023-10-24 17:13:12 -0400

    Added characters to brute force

--- intellij/src/JavaUserInterface.java
+++ intellij/src/JavaUserInterface.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b69f1cdbec6f4672da1dd270b120c7eafa9786af238c06c3cb0ac9ee3a3fa2af
-size 8120
+oid sha256:b44b09a4e138d59ce4a4595b09296f91d057131f4f27a081febdea048870a03c
+size 8159
