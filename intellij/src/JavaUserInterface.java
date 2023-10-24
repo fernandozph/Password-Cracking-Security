cc614b0dc294007d4f5222172bcf6c788812a91b
Fernando <nandoz1ph@gmail.com>
2023-10-24 15:45:12 -0400

    Added empty salt values

--- intellij/src/JavaUserInterface.java
+++ intellij/src/JavaUserInterface.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2829950f1211d8f43d570b94341ed7563c4eea7a6a00a5a2555531f86da8c50
-size 8021
+oid sha256:b69f1cdbec6f4672da1dd270b120c7eafa9786af238c06c3cb0ac9ee3a3fa2af
+size 8120
--- intellij/src/main.java
+++ intellij/src/main.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fca7a981dc9cceaec7beb36a3b46954f4c63f2e1dc141ad47e08fab3926f4afd
-size 1756
+oid sha256:6f56573486a861a4c3e9d6b83c58976baba715951c284cd61b04b3135de0362e
+size 1719
