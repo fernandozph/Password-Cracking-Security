2487bcdb2cfd928a2174c250470274c01d2cba45
John Gador <jgador@acl203.cs.vassar.edu>
2023-10-23 22:13:17 -0400

    fixed MVC based on Rui's feedback

--- intellij/src/Controller.java
+++ intellij/src/Controller.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3af8b11e34f242f75797caac92f38a76b06985de2d907b523382961aede8798e
-size 3942
+oid sha256:d12aa2e18211bdad40802aaa427997e9c62c093fcb52c7d0ef9832450c8214e1
+size 1790
--- intellij/src/JavaUserInterface.java
+++ intellij/src/JavaUserInterface.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5c5daf13ca7355189ba0493739f3a3c961fcba890e5cae2d6fb463121ee0f0d
-size 5638
+oid sha256:c2829950f1211d8f43d570b94341ed7563c4eea7a6a00a5a2555531f86da8c50
+size 8021
