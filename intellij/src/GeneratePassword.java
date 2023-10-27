fa4d4256ca16253a43950235d38e4f50cd76c048
John Gador <jgador@bpascal.cs.vassar.edu>
2023-10-26 20:40:48 -0400

    Fixed the probability of numbers and symbols showing up

--- intellij/src/GeneratePassword.java
+++ intellij/src/GeneratePassword.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:832ab68c3f42ae21685fd3c7f3bd40b2ae6ed1645ea5f5dcc0e682ba9f2f02a3
-size 2466
+oid sha256:0baf3958846b00bb15609f3bb993b52f4432e117fec693c8ba63a344fc39bb11
+size 4363
