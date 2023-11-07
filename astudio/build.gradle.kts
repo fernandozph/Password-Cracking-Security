ce070dc2de754010c5e856032f4051a638f03f3a
jgador-vassar <112576673+jgador-vassar@users.noreply.github.com>
2023-11-07 15:34:58 -0500

    viewBinding

--- astudio/build.gradle.kts
+++ astudio/build.gradle.kts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d844a26810ee861d3dd1fd0b4c73a889b44002bb18c776649d0552ed0ae8d36
+oid sha256:2df5bd1946039fd989e14de08cb8a6a69da5c825290024a8c3fcb22bb6af4264
 size 173
