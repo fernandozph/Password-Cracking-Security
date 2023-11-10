94ca8823933f9f38fd0cc8821c7e92f7ebb5b987
jgador-vassar <112576673+jgador-vassar@users.noreply.github.com>
2023-11-10 12:20:15 -0500

    NewPassword Fragment interface and class

---
+++ astudio/app/src/main/java/com/example/passwordapp/view/INewPasswordView.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:85f08454d9f93678b0efa3e5ac67c75e4e1ec80a234bef863ea5bf4dcf8e233e
+size 162
---
+++ astudio/app/src/main/java/com/example/passwordapp/view/NewPasswordFragment.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2be68ebddfa7bd38f01bb409a5454ed2f071ab24579475c85ec07503c01d497d
+size 1332
