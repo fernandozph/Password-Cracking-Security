4635a80bb172b1949e79b4e067fe5e830c4c2633
jgador-vassar <112576673+jgador-vassar@users.noreply.github.com>
2023-11-09 22:05:19 -0500

    Changed the name of the root view

--- astudio/app/src/main/java/com/example/passwordapp/controller/ControllerActivity.java
+++ astudio/app/src/main/java/com/example/passwordapp/controller/ControllerActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e7152a783b7509485ba97d7efec113fb3499ad635bce40e5fd14128454a54ce
-size 626
+oid sha256:b09ac0141ed9a5c322ed801debf1c3d55dfc77d87c84a13a597ad6607d0be68e
+size 701
