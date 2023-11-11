3f320729f042821d2d08c32c113ecf85015c7b19
jgador-vassar <112576673+jgador-vassar@users.noreply.github.com>
2023-11-11 14:03:22 -0500

    Made some fields final according to Android Studio's suggestions

--- astudio/app/src/main/java/com/example/passwordapp/model/GeneratePassword.java
+++ astudio/app/src/main/java/com/example/passwordapp/model/GeneratePassword.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a50d394713a95751d9b1ba641dae7f9bcc6849b4cd0182015d3f70a506d50d2
-size 4437
+oid sha256:5b28c0cac3dd0627caa0c0adbea67efe71367f293904c3cdb4e306dd51e4187e
+size 4481
