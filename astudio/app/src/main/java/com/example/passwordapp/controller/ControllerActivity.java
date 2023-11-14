508724c6968768658b120197f76bf5fec37513dd
Fernando <nandoz1ph@gmail.com>
2023-11-14 18:40:17 -0500

    Fixes for ControllerActivity.java calls to CrackingPassword fragment

--- astudio/app/src/main/java/com/example/passwordapp/controller/ControllerActivity.java
+++ astudio/app/src/main/java/com/example/passwordapp/controller/ControllerActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2095c4796d0e8d3812582fabaa200d9fc8b1401bbd400a1d995e3575c1f16d37
-size 5668
+oid sha256:d8faedf4ad3b07f04cfff8ecda5ceeaa439005b71511f3e204d61fddf693602b
+size 6253
