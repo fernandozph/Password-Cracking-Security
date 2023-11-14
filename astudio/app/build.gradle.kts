c680d82dcb892d5899275f807a0e5b5264495048
jgador-vassar <112576673+jgador-vassar@users.noreply.github.com>
2023-11-14 17:31:06 -0500

    animations disabled in gradle

--- astudio/app/build.gradle.kts
+++ astudio/app/build.gradle.kts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be8b40d47962ead7312dd89b1aad0dd05917f7110645a1fdb1855f6288631351
-size 1333
+oid sha256:9103d156d9023ffaaf512e81b0de34d573436a0da23e960d627e8e8e40fa49d1
+size 1392
