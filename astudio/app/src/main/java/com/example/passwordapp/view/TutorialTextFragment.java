f5d879981e83367f6ade32fdcf671e6d011335d3
Fernando <nandoz1ph@gmail.com>
2023-11-15 21:37:51 -0500

    Added "use app" tutorial text.

--- astudio/app/src/main/java/com/example/passwordapp/view/TutorialTextFragment.java
+++ astudio/app/src/main/java/com/example/passwordapp/view/TutorialTextFragment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5193a61c785d8ec561cbe4532e2f98f3e62fe4bcc6272df9ba96c44de914348c
-size 5067
+oid sha256:e46f4ec24a8dce79c02be0562f6219918e86b559517dc2ba2cabd3904706b2fb
+size 5025
