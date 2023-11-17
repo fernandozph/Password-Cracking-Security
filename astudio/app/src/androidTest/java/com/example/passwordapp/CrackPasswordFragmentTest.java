24c1ccfa28545ed8a0d7f4cbb049562256c2c0ca
Fernando <nandoz1ph@gmail.com>
2023-11-16 22:18:21 -0500

    Removed unnecessary sleeps and imports

--- astudio/app/src/androidTest/java/com/example/passwordapp/CrackPasswordFragmentTest.java
+++ astudio/app/src/androidTest/java/com/example/passwordapp/CrackPasswordFragmentTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8b757a4118bc46ef1cc73acc11328b090c1efdf71cce423af5940b9cf419b3f
-size 4210
+oid sha256:656d207871b177a19e3a8a80a4985caa54585181ca48d1628a860e74bdf5a0b1
+size 4002
