72bd352b4095120f5c639fc4214c2b2d7296bd6f
Fernando <nandoz1ph@gmail.com>
2023-11-17 22:41:13 -0500

    Added clarifying comments on IndexOutOfBoundsException catch, and changed return string when unable to find the password (>16 chars)

--- astudio/app/src/main/java/com/example/passwordapp/model/BruteForce.java
+++ astudio/app/src/main/java/com/example/passwordapp/model/BruteForce.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f17300bbbfabb095f98ade9a33b8fdac8d027eaea719366ee650bdf78458210f
-size 4972
+oid sha256:ac5d2d7dc847ebc996f302258dcdaf6d208e18f8d13ad7b8ab8c0751278af37d
+size 5622
