dcac8cd71a0a1593b8a90c062765dd96beeb4390
Fernando <nandoz1ph@gmail.com>
2023-11-20 21:04:13 -0500

    Clarified comments and added thread shutdown

--- astudio/app/src/main/java/com/example/passwordapp/controller/ControllerActivity.java
+++ astudio/app/src/main/java/com/example/passwordapp/controller/ControllerActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92192bf3b34b5347e9ce31feba0130cbb211ceb6351a9f1b6b85f99e2c26b585
-size 10664
+oid sha256:3f99b0e98a6b9ce99efb8e4c9d10e3d3db8bcb73751537b1048449344e82f743
+size 10680
