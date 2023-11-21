e4bc4ed299c00ff81e81ec768e5e683bce2aca43
Fernando <nandoz1ph@gmail.com>
2023-11-20 22:54:02 -0500

    it works

--- astudio/app/src/main/java/com/example/passwordapp/controller/ControllerActivity.java
+++ astudio/app/src/main/java/com/example/passwordapp/controller/ControllerActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e20ed0fc088f5a7a59ac3601a1f4374cc9087ac2735fab9cbdcb621f326ff4c7
-size 11226
+oid sha256:f099b5b5b055413081738e50a87bc0828b7237ae8cb20162cf13a8c25ea07f23
+size 11385
