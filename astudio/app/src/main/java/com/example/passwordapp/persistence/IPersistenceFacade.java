d8ab577118303ae91beea7b83e6651a5a0ccdfd8
jgador-vassar <112576673+jgador-vassar@users.noreply.github.com>
2023-11-21 15:15:03 -0500

    Persistence interface and local storage class

---
+++ astudio/app/src/main/java/com/example/passwordapp/persistence/IPersistenceFacade.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ffabf474ee70c80bdb6bd7fa4d44ebfdcd2da21a9eb8575689e725c1a870a422
+size 122
---
+++ astudio/app/src/main/java/com/example/passwordapp/persistence/LocalStorageFacade.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fd18a62bce1a7c66bfd566bb53550c2b86304b14c265a16336065e1df8632ee3
+size 82
