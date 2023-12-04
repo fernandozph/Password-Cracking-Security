2ced8040ff50ebecacfc33354ccd5343e2103ecd
Fernando <nandoz1ph@gmail.com>
2023-12-03 19:11:12 -0500

    Made ExecutorService an attribute in ControllerActivity, it will stay running the entire time the app is up.

--- astudio/app/src/main/java/com/example/passwordapp/controller/ControllerActivity.java
+++ astudio/app/src/main/java/com/example/passwordapp/controller/ControllerActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87ba8973c620046421dd605c0e1d97fb3a2c4da613563452ff82c9c98e0f1913
-size 11283
+oid sha256:04760cb77e72ab69671316b4f6b9bc5e277bd0bcc1b3c5569eead751dbbfaa43
+size 10864
