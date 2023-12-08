94bb24461c2d65d2862f192443a007dd5f937ad6
Fernando <nandoz1ph@gmail.com>
2023-12-07 21:22:46 -0500

    Fixed tests and adjusted to have different methods for hashing and password cracking tests

--- astudio/app/src/androidTest/java/com/example/passwordapp/CrackPasswordFragmentTest.java
+++ astudio/app/src/androidTest/java/com/example/passwordapp/CrackPasswordFragmentTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96966d451f5c075a1244c99044b1b2dd8abc9799e21c9d1da49bf54ee5bd01e0
-size 7056
+oid sha256:9f5159444eee53348b98b9030f28bcf57cb23967d1bf14cb6089bd744348c1e4
+size 7875
