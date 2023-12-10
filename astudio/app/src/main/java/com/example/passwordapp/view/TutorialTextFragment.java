aac4124c64c5a51c4332e34ee5e520577ec9d860
jgador-vassar <112576673+jgador-vassar@users.noreply.github.com>
2023-12-09 21:26:19 -0500

    Dynamic view of the fragments

--- astudio/app/src/main/java/com/example/passwordapp/view/TutorialTextFragment.java
+++ astudio/app/src/main/java/com/example/passwordapp/view/TutorialTextFragment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84223d00865f3ff3c404a5d1c2798726da561d2b36a6cb529c116150456f779f
-size 5922
+oid sha256:62c788866683ed99cfea3eec5fbbb71949bda7e2d0daecad3c30696fa23f6cb7
+size 7630
