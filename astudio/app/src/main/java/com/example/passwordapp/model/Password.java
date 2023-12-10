13c050662e0a1c886eb925cb1fd8d7bd94f98396
Fernando <nandoz1ph@gmail.com>
2023-12-10 15:45:22 -0500

    Streamlined password hashing to remove repetitive code

--- astudio/app/src/main/java/com/example/passwordapp/model/Password.java
+++ astudio/app/src/main/java/com/example/passwordapp/model/Password.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3dbe6249ad9b0f842ac21982cb095baece79cbd4a48f175304f7c921331c58fe
-size 3941
+oid sha256:10b778b173a4099d221d640cc9900885da6c90b041e3f2e01031a048a74e8fca
+size 3682
